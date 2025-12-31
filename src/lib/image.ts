905b3885aa8ee7929e599f2328b52c35c8cff2ad
niklas7cc <niklas.7cc@gmail.com>
2025-12-31 15:38:36 +0100

    Fix issue

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -1,9 +1,8 @@
 import imageUrlBuilder from '@sanity/image-url'
 import { client } from './sanity'
-import type { SanityImageSource } from '@sanity/image-url/lib/types/types.js'
 
 const builder = imageUrlBuilder(client)
 
-export function urlFor(source: SanityImageSource) {
+export function urlFor(source: any) {
   return builder.image(source)
 }
